6567555da64e87e5c324fc886927e4ec51fc974d
iraiyan50 <128201651+iraiyan50@users.noreply.github.com>
2024-04-28 00:32:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const video = document.getElementById("video");
 
-webCam();
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/test/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/test/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/test/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/test/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/test/models"),
+]).then(webCam);
 
 function webCam() {
   navigator.mediaDevices
@@ -16,3 +22,36 @@ function webCam() {
     });
 }
 
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+
+  setInterval(async () => {
+    const detection = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions().withAgeAndGender();
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    const resizedWindow = faceapi.resizeResults(detection, {
+      height: video.height,
+      width: video.width,
+    });
+
+    faceapi.draw.drawDetections(canvas, resizedWindow);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedWindow);
+    faceapi.draw.drawFaceExpressions(canvas, resizedWindow);
+
+    resizedWindow.forEach((detection) => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: Math.round(detection.age) + " year old " + detection.gender,
+      });
+      drawBox.draw(canvas);
+    });
+
+    console.log(detection);
+  }, 100);
+});
